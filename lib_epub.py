adc278ce9a8e21c38ebd9527f24e23f6d3664731
Nathaniel Gunby <nate.gunby@gmail.com>
2016-12-13 16:59:26 +1300

    Add function to find epub content files and test for same

--- lib_epub.py
+++ lib_epub.py
@@ -1,12 +1,28 @@
 import xml.etree.ElementTree as ET
 import os.path
-epub_xmlns = '{urn:oasis:names:tc:opendocument:xmlns:container}'
+root_xmlns = '{urn:oasis:names:tc:opendocument:xmlns:container}'
+manifest_xmlns = '{http://www.idpf.org/2007/opf}'
 
 def iter_rootfiles(epub_dir_name):
     container_tree = ET.parse(os.path.join(epub_dir_name, 'META-INF', 'container.xml'))
     try:
         for i in container_tree.getroot().find('rootfiles').findall('rootfile'):
-            yield i.get('full-path')
+            yield os.path.join(epub_dir_name, i.get('full-path'))
     except AttributeError:
-        for i in container_tree.getroot().find(epub_xmlns + 'rootfiles').findall(epub_xmlns + 'rootfile'):
-            yield i.get('full-path')
+        for i in container_tree.getroot().find(root_xmlns + 'rootfiles').findall(root_xmlns + 'rootfile'):
+            yield os.path.join(epub_dir_name, i.get('full-path'))
+
+def iter_manifest(opf_filename):
+    base_dirname = os.path.dirname(opf_filename)
+    opf_tree = ET.parse(opf_filename)
+    root = opf_tree.getroot()
+    try:
+        for i in root.find('manifest').findall('item'):
+            href = i.get('href')
+            if os.path.splitext(href)[1] in ('.html', '.xhtml'):
+                yield os.path.join(base_dirname, href)
+    except AttributeError:
+        for i in root.find(manifest_xmlns + 'manifest').findall(manifest_xmlns + 'item'):
+            href = i.get('href')
+            if os.path.splitext(href)[1] in ('.html', '.xhtml'):
+                yield os.path.join(base_dirname, href)
--- test_lib_epub.py
+++ test_lib_epub.py
@@ -15,7 +15,14 @@ class base(unittest.TestCase):
 class TestIterRootfiles(base):
     def test_simple(self):
         ret = [i for i in lib_epub.iter_rootfiles(self.epub.new_dir_name)]
-        self.assertEqual(ret, ['content.opf'])
+        self.assertEqual(ret, [self.epub.new_dir_name + '/content.opf'])
+
+class TestIterManifest(base):
+    def test_simple(self):
+        tmp = [i for i in lib_epub.iter_rootfiles(self.epub.new_dir_name)]
+        ret = [i for i in lib_epub.iter_manifest(tmp[0])]
+        self.assertEqual(ret[0], self.epub.new_dir_name + '/index_split_000.html')
+        self.assertEqual(len(ret), 68)
 
 if __name__ == '__main__':
     unittest.main()
