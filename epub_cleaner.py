fe8a6076374011364749795cc8c20a7a2b32c420
Nathaniel Gunby <nate.gunby@gmail.com>
2016-12-13 14:55:26 +1300

    Added EpubCleaner.setup and tests

--- epub_cleaner.py
+++ epub_cleaner.py
@@ -1,4 +1,7 @@
 import os.path
+import zipfile
+from shutil import copytree
+from os import mkdir
 
 class EpubCleaner(object):
     def __init__(self, epub_name):
@@ -9,3 +12,9 @@ class EpubCleaner(object):
         self.old_dir_name = os.path.join(root_dir_name, old_base_name)
         self.new_dir_name = os.path.join(root_dir_name, new_base_name)
         self.new_epub_name = os.path.join(root_dir_name, new_base_name + ext_name)
+
+    def setup(self):
+        old_epub_zip = zipfile.ZipFile(self.old_epub_name)
+        mkdir(self.old_dir_name)
+        old_epub_zip.extractall(self.old_dir_name)
+        copytree(self.old_dir_name, self.new_dir_name)
--- test_epub_cleaner.py
+++ test_epub_cleaner.py
@@ -1,7 +1,9 @@
+from shutil import rmtree
 import unittest
 import epub_cleaner
+import os.path
 
-class TestCleanSetup(unittest.TestCase):
+class TestCleanInit(unittest.TestCase):
     def test_simple(self):
         tmp = epub_cleaner.EpubCleaner('Dracula.epub')
         self.assertEqual(tmp.old_epub_name, 'Dracula.epub')
@@ -22,6 +24,19 @@ class TestCleanSetup(unittest.TestCase):
         self.assertEqual(tmp.old_dir_name, '/../Dracula')
         self.assertEqual(tmp.new_epub_name, '/../clean_Dracula.epub')
         self.assertEqual(tmp.new_dir_name, '/../clean_Dracula')
+
+class TestCleanSetup(unittest.TestCase):
+    def test_simple(self):
+        tmp = epub_cleaner.EpubCleaner('test/HenryVI.epub')
+        tmp.setup()
+        self.assertTrue(os.path.exists('test/HenryVI'))
+        self.assertTrue(os.path.exists('test/clean_HenryVI'))
+        self.assertFalse(os.path.isfile('test/HenryVI'))
+        self.assertFalse(os.path.isfile('test/clean_HenryVI'))
+
+    def tearDown(self):
+        rmtree('test/HenryVI')
+        rmtree('test/clean_HenryVI')
 
 if __name__ == '__main__':
     unittest.main()
